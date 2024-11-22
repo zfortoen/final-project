17bbedaee29aae0b333e551034933bab6bdcbf86
Lira Johnson <zaida>
2024-11-22 14:54:32 +0200

    added content

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-const navbar = document.querySelector('#navbar__logo')
 
 //display mobile menu
 const mobileMenu = ()  => {
